d2d3489c2405180d84b1182fd0a5404a99d29b6d
Brandon <brantauber@Brandons-MacBook-Pro-3.local>
2014-03-14 10:00:56 -0400

    Initial Commit

---
+++ Quiz5/QuizAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  QuizAppDelegate.h
+//  Quiz5
+//
+//  Created by Brandon on 3/14/14.
+//  Copyright (c) 2014 Brandon Tauber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QuizAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
